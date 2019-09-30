5c8a7ebe8689f756469ab5971d729294d40829a0
project440 <rrj.pandey@gmail.com>
2019-09-29 18:26:52 -0700

    ssh config initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+package 'sshd'
+
+service 'sshd' do
+  action [:enable, :start]
+end
+
+file '/etc/ssh/ssh_config' do # ~FC033
+  PasswordAuthentication no
+end
+user 'vagrant' do
+  password 'vagrant'
+end
